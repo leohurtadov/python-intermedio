96f805b5f363340b6832ba2f3229d770b7fa056d
Leonardo Hurtado <leonardo.hurtadov@gmail.com>
2021-04-29 09:19:51 -0600

    dicts comperhensions

---
+++ dict_comperhensions.py
@@ -0,0 +1,15 @@
+def main():
+    # my_dict = {}
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         my_dict[i] = i**3
+    ''' dict comperhensions
+    {key: value for value in iterable if condition}
+    '''
+    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+
+    print(my_dict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ dict_comperhensions_challenge.py
@@ -0,0 +1,8 @@
+def main():
+
+    my_dict = {i: round(i**0.5, 2) for i in range(1, 1001)}
+    print(my_dict)
+
+
+if __name__ == '__main__':
+    main()
