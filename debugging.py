68d43b6e227c294472de798716e47c39ef7c8975
Leonardo Hurtado <leonardo.hurtadov@gmail.com>
2021-05-03 09:34:29 -0600

    Uso de excepciones

--- debugging.py
+++ debugging.py
@@ -1,11 +1,19 @@
 def divisors(num):
-    divisors = [i for i in range(1, num + 1) if num % i == 0]
-    return divisors
+    try:
+        if num < 0 or num == 0:
+            raise ValueError('Ingresa un numero mayor a cero')
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        return ve
 
 def main():
-    num = int(input('Ingresa un numero: '))
-    print(divisors(num))
-    print('Ok...')
+    try:
+        num = int(input('Ingresa un numero: '))
+        print(divisors(num))
+        print('Ok...')
+    except ValueError:
+        print('Debes ingresar un numero entero')
 
 
 if __name__ == '__main__':
